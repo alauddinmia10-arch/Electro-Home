53b5ca99d4cab1881319a6cff3e8cf00fd88bdd6
Md Alauddin Miah <alauddinmia10@Gmail.com>
2026-08-05 00:03:00 +0600

    Hardcode Apache port to 10000 to fix crash

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Cache Laravel configuration
+echo "Caching config..."
 php artisan config:cache
+echo "Caching routes..."
 php artisan route:cache
+echo "Caching views..."
 php artisan view:cache
 
 # Run database migrations
+echo "Running migrations..."
 php artisan migrate --force
 
-# Start Apache in the foreground
+echo "Starting Apache..."
 apache2-foreground
