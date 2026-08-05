5f084a7ee482c78e07cf1a53b78fbdc12028eae2
Md Alauddin Miah <alauddinmia10@Gmail.com>
2026-08-05 10:21:05 +0600

    Fix Cloudinary configuration array and environment variables

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,7 +49,14 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
-            'url' => env('CLOUDINARY_URL'),
+            'cloud' => [
+                'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+                'api_key' => env('CLOUDINARY_API_KEY'),
+                'api_secret' => env('CLOUDINARY_API_SECRET'),
+            ],
+            'url' => [
+                'secure' => true,
+            ],
         ],
 
         's3' => [
