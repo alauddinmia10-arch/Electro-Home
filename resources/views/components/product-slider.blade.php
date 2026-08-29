b21d69a08eb9d3167de57ef1f74592a6630a739a
alauddinmia10-arch <alauddinmia10@gmail.com>
2026-08-29 04:43:33 -0700

    Fix slider navigation arrows to touch container borders and adjust cart badge positioning

--- resources/views/components/product-slider.blade.php
+++ resources/views/components/product-slider.blade.php
@@ -31,7 +31,7 @@
 
     <div class="relative group">
         {{-- Left Arrow --}}
-        <div class="absolute -left-5 md:-left-4 xl:-left-12 top-1/2 -translate-y-1/2 z-50" x-cloak x-show="showLeft">
+        <div class="absolute left-0 top-1/2 -translate-y-1/2 z-50" x-cloak x-show="showLeft">
             <button @click="scrollLeft" 
                     style="animation: float-pulse-icon 2s infinite ease-in-out; background: none !important; border: none !important; box-shadow: none !important;"
                     class="p-0 flex items-center justify-center focus:outline-none text-gray-700 hover:text-[var(--color-trust-blue)] transition-colors">
@@ -49,7 +49,7 @@
         </div>
 
         {{-- Right Arrow --}}
-        <div class="absolute -right-5 md:-right-4 xl:-right-12 top-1/2 -translate-y-1/2 z-50" x-cloak x-show="showRight">
+        <div class="absolute right-0 top-1/2 -translate-y-1/2 z-50" x-cloak x-show="showRight">
             <button @click="scrollRight" 
                     style="animation: float-pulse-icon 2s infinite ease-in-out; background: none !important; border: none !important; box-shadow: none !important;"
                     class="p-0 flex items-center justify-center focus:outline-none text-gray-700 hover:text-[var(--color-trust-blue)] transition-colors">
--- resources/views/livewire/cart-icon.blade.php
+++ resources/views/livewire/cart-icon.blade.php
@@ -21,11 +21,11 @@ new class extends Component {
 };
 ?>
 <a href="{{ route('cart') }}" class="flex items-center gap-2 p-2 -mr-2 md:mr-0 group text-gray-600 hover:text-[var(--color-trust-blue)] transition-colors relative" x-data="{ cartCount: {{ $count }}, cartSubtotal: {{ $subtotal }} }" @cart-updated-optimistic.window="cartCount += $event.detail.qty_change; cartSubtotal += $event.detail.amount" @cart-updated.window="if($event.detail.subtotal !== undefined) { cartCount = $event.detail.count; cartSubtotal = $event.detail.subtotal; }">
-    <div class="relative mr-2">
+    <div class="relative ml-2">
         <svg class="w-6 h-6" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M12 7C12 7.55 11.55 8 11 8C10.45 8 10 7.55 10 7V5H8C7.45 5 7 4.55 7 4C7 3.45 7.45 3 8 3H10V1C10 0.45 10.45 0 11 0C11.55 0 12 0.45 12 1V3H14C14.55 3 15 3.45 15 4C15 4.55 14.55 5 14 5H12V7ZM4.01 19C4.01 17.9 4.9 17 6 17C7.1 17 8 17.9 8 19C8 20.1 7.1 21 6 21C4.9 21 4.01 20.1 4.01 19ZM16 17C14.9 17 14.01 17.9 14.01 19C14.01 20.1 14.9 21 16 21C17.1 21 18 20.1 18 19C18 17.9 17.1 17 16 17ZM14.55 12H7.1L6 14H17C17.55 14 18 14.45 18 15C18 15.55 17.55 16 17 16H6C4.48 16 3.52 14.37 4.25 13.03L5.6 10.59L2 3H1C0.45 3 0 2.55 0 2C0 1.45 0.45 1 1 1H2.64C3.02 1 3.38 1.22 3.54 1.57L7.53 10H14.55L17.94 3.87C18.2 3.39 18.81 3.22 19.29 3.48C19.77 3.75 19.95 4.36 19.68 4.84L16.3 10.97C15.96 11.59 15.3 12 14.55 12Z" fill="currentColor"/>
         </svg>
-        <span x-show="cartCount > 0" x-text="cartCount" wire:ignore class="absolute text-red-600 font-bold text-center" style="display: none; top: -8px; right: -8px; font-size: 22px; line-height: 1;">
+        <span x-show="cartCount > 0" x-text="cartCount" wire:ignore class="absolute text-red-600 font-bold text-center" style="display: none; top: -8px; right: 22px; font-size: 22px; line-height: 1;">
             {{ $count }}
         </span>
     </div>
